2297d9740e73f1ca0692a01fb3e33359e7b6a0b6
Jose <jomaos0711@gmail.com>
2026-05-17 12:45:38 -0500

    .

--- 09-Funciones/js/13-app.js
+++ 09-Funciones/js/13-app.js
@@ -1,4 +1,7 @@
 const reproductor = {
+
+    cancion: " ",
+
     reproducir: (nombre = '') => console.log(`Reproduciendo ${nombre}`),
 
     pausar: () => {console.log(`Pausando`);},
@@ -7,7 +10,17 @@ const reproductor = {
 
     crearPlaylist: (playname = " ") => {console.log(`Playlist ${playname} creada`);},
     
-    reproducirplaylist: (playname = '')=> {console.log(`Reproduciendo ${playname}`);}
+    reproducirplaylist: (playname = '')=> {console.log(`Reproduciendo ${playname}`);},
+
+    set nuevaCancion(cancion) {
+        this.cancion = cancion;
+        console.log(`Agregando ${cancion}`);
+    },
+
+    get obtenerCancion() {
+        console.log(`La cancion actual es ${this.cancion}`);
+    }
+
 }
 
 reproductor.reproducir("Doma");
