04ffb4b456f8f6a30627678482a5c41c35bcd94b
Sergio Torres <sergio.arango166@gmail.com>
2026-05-22 18:03:56 -0500

    every (AND) some(OR) y .concat o ...

---
+++ 12-ArrayMethods/js/07-app.js
@@ -0,0 +1,12 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+const meses2 = ['Agosto', 'Septiembre'];
+const meses3 = ['Octubre', 'Noviembre', 'Diciembre'];
+
+// .concat
+const resultado = meses.concat(meses, meses2);
+
+//... sirven para copiar lo que este asignado 
+
+
+resultado2 = [...meses, ...meses2];
+
