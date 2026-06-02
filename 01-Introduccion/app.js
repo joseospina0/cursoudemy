d2c165e74a8c27f20c39c9677a66508632a2c21c
Sergio Torres <sergio.arango166@gmail.com>
2026-06-02 17:29:23 -0500

    ejercicio propio de html :3

--- 01-Introduccion/app.js
+++ 01-Introduccion/app.js
@@ -1,2 +1,27 @@
-console.log('Hola Mundo')
+const random = document.querySelector('.btn-rndm');
+
+/* random.addEventListener('click',  () => {
+    let on = random.classList.contains('btn-active')
+    if(on){
+        random.classList.remove('btn-active')
+    }else{
+        random.classList.add('btn-active')
+    }
+}) */
+
+
+// Ejemplo con 2 add even listener
+/* random.addEventListener('mouseenter', añadirColor);
+
+random.addEventListener('mouseleave', quitarColor);
+
+function añadirColor(){
+    random.classList.add('btn-active');
+}
+
+function quitarColor(){
+    random.classList.remove('btn-active');
+} */
+
+
 
